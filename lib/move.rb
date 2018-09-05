5b1d120448bdd88b9557a0c97a4d987890424a4a
Felix Godfree <felixfgodfree@gmail.com>
2018-09-05 17:50:41 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -10,6 +10,6 @@ def input_to_index(number)
   number.to_i -= 1
 end
 
-def move(board, input_to_index(input), character = "X")
+def move(board, input, character = "X")
   board[input_to_index(input)] = character
 end
